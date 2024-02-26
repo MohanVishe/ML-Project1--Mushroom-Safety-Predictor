90eb1f24a6fcd9ecf985b34ad1fd203ce212019f
Mohan Vishe <mohanvishe1999@gmail.com>
2024-02-26 17:33:56 +0530

    Requirements and Setup files

---
+++ init_setup.sh
@@ -0,0 +1,26 @@
+# To install Environment with all packages directly:
+# 1. open gitbash in terminal
+# 2. use command : bash init_setup.sh
+
+echo [$(date)]:"START"
+
+
+echo "Creating Environment and installing Packages"
+# conda env create -p MLp1Env --file "requirements/environment.yml"
+conda create -p MLp1Env python -y
+
+
+echo "Activating Environment"
+source activate ./MLp1Env
+pip install -r "requirements/requirements.txt"
+
+
+echo "Create Folder Structure"
+python "requirements\template.py"
+
+
+echo "Installing Local Package"
+python setup.py install
+
+
+echo [$(date)]: "END"
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="MushroomSafetyPredictor",
+    author="Mohan Vishe",
+    email="mohanvishe1999@gmail.com",
+    packages=find_packages()
+)
